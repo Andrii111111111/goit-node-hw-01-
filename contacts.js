023ef381f62e8ee1425c413d5de999e79cb8f188
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-12-14 23:10:51 +0100

    punkt 2

--- contacts.js
+++ contacts.js
@@ -1,9 +1,19 @@
 import fs from "fs/promises";
 import detectFileEncoding from "detect-file-encoding-and-language";
+import path from "path";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+// ./db/contacts.json
 
 export const listContacts = async () => {
-  const contactsPath = "./db/contacts.json";
-  const { encoding } = await detectFileEncoding(contactsPath);
-  const data = await fs.readFile(contactsPath, encoding);
-  console.log(data);
+  // const { encoding } = await detectFileEncoding(contactsPath);
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+};
+
+export const getContactsById = async (id) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === id);
+  return result;
 };
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-import { listContacts } from "./contacts.js";
+import * as contacts from "./contacts.js";
 
-listContacts()
-  .then((contacts) => console.log(contacts))
-  .catch((error) => console.log(error.message));
+// const argv = require("yargs").argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactsById(id);
+      return console.log(oneContact);
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction({ action: "get", id: "AeHIrLTr6JkxGE6SN-0Rw" });
